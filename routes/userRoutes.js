961117c4a14a384d741d9896a0604ec7b2db084d
Shivamk2745 <shivamkashyap0126@gmail.com>
2024-01-01 19:02:21 +0530

    routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const [
   userCurrent,
 ] = require("../controller/userController");
 const validateToken = require("../middleware/validToken");
-
+console.log("for validation");
 router.post("/register", userRegister);
 router.post("/login", userLogin);
 router.get("/current", validateToken, userCurrent);
